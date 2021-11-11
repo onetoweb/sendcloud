8063d6d6c8eee86fa63030ef74e6e547e0bb3bcb
Jonathan van 't Ende <jvantende@onetoweb.nl>
2021-11-11 16:34:08 +0100

    expand example.php, add get, post & put methods to client

--- example.php
+++ example.php
@@ -11,8 +11,18 @@ $apiSecret = 'api_secret';
 // get client
 $client = new Client($apiKey, $apiSecret);
 
+// get parcels
+$parcels = $client->get('parcels');
+
+// follow next page link
+$parcels = $client->get($parcels['next']);
+
+// get parcel
+$parcelId = 42;
+$parcel = $client->get("parcels/$parcelId");
+
 // create parcel
-$parcel = $client->request(Client::METHOD_POST, 'parcels', [
+$parcel = $client->post('parcels', [
     'parcel' => [
         'name' => 'John Doe',
         'company_name' => 'Sendcloud',
@@ -38,15 +48,237 @@ $parcel = $client->request(Client::METHOD_POST, 'parcels', [
     ]
 ]);
 
+// create multiple parcels
+$parcels = $client->post('parcels', [
+    'parcels' => [[
+        'name' => 'John Doe',
+        'company_name' => 'Sendcloud',
+        'address' => 'Insulindelaan',
+        'house_number' => '115',
+        'city' => 'Eindhoven',
+        'postal_code' => '5642CV',
+        'telephone' => '+31612345678',
+        'request_label' => true,
+        'email' => 'john@doe.com',
+        'data' => [],
+        'country' => 'NL',
+        'shipment' => [
+            'id' => 8
+        ],
+        'weight' => '10.000',
+        'order_number' => '1234567890',
+        'insured_value' => 2000,
+        'total_order_value_currency' => 'GBP',
+        'total_order_value' => '11.11',
+        'quantity' => 1,
+        'shipping_method_checkout_name' => 'DHL Express Domestic'
+    ], [
+        'name' => 'John Doe',
+        'company_name' => 'Sendcloud',
+        'address' => 'Insulindelaan',
+        'house_number' => '115',
+        'city' => 'Eindhoven',
+        'postal_code' => '5642CV',
+        'telephone' => '+31612345678',
+        'request_label' => true,
+        'email' => 'john@doe.com',
+        'data' => [],
+        'country' => 'NL',
+        'shipment' => [
+            'id' => 8
+        ],
+        'weight' => '8.000',
+        'order_number' => '1234567891',
+        'insured_value' => 500,
+        'total_order_value' => '99.99',
+        'total_order_value_currency' => 'EUR',
+        'quantity' => 1,
+        'shipping_method_checkout_name' => 'DPD Classic'
+    ]]
+]);
+
+// pack & go creation request
+$parcel = $client->post('parcels', [
+    'parcel' => [
+        'name' => 'Rob van den Heuvel',
+        'company_name' => 'Sendcloud',
+        'address' => 'Insulindelaan',
+        'house_number' => '115',
+        'city' => 'Eindhoven',
+        'postal_code' => '5642CV',
+        'country' => 'NL',
+        'telephone' => '+31612345678',
+        'request_label' => false,
+        'email' => 'rob@sendcloud.nl',
+        'order_number' => '1234567890',
+        'parcel_items' => [
+            [
+                'description' => 'T-Shirt',
+                'quantity' => 2,
+                'weight' => '0.3',
+                'sku' => 'sku1',
+                'value' => '300',
+                'properties' => [
+                    'Size' => 'Medium',
+                    'Color' => 'Blue'
+                ]
+            ], [
+                'description' => 'Trousers',
+                'quantity' => 1,
+                'weight' => '0.3',
+                'sku' => 'sku2',
+                'value' => '100',
+                'properties' => [
+                    'Size' => '58',
+                    'Color' => 'Black'
+                ]
+            ]
+        ]
+    ]
+]);
+
+// update parcel
+$parcelId = 42;
+$parcel = $client->put('parcels', [
+    'parcel' => [
+        'id' => $parcelId,
+        'name' => 'Anna Tester',
+        'company_name' => 'Summer Co',
+        'request_label' => true
+    ]
+]);
+
+// cancel / delete a parcel
+$parcelId = 42;
+$status = $client->post("parcels/$parcelId/cancel");
+
+// return portal url
+$parcelId = 42;
+$result = $client->get("parcels/$parcelId/return_portal_url");
+
+// get parcel document
+$parcelId = 42;
+$result = $client->get("parcels/$parcelId/documents/pdf", [
+    'format' => 'pdf',
+    'dpi' => '72'
+]);
+
+// get parcel statuses
+$statuses = $client->get('parcels/statuses');
+
+// create parcels report
+$result = $client->post('reporting/parcels', [
+    'fields' => [
+        'parcel_id',
+        'direction',
+        'carrier_code',
+        'arrived_at'
+    ],
+    'filters' => [
+        'direction' => 'incoming',
+        'integration_id' => 0,
+        'updated_after' => '2019-08-24T14:15:22Z',
+        'updated_before' => '2019-08-24T14:15:22Z',
+        'announced_after' => '2019-08-24T14:15:22Z',
+        'announced_before' => '2019-08-24T14:15:22Z'
+    ]
+]);
+
+// get parcel report
+$reportId = 42;
+$report  = $client->get("reporting/parcels/$reportId");
+
+// get returns
+$returns = $client->get('returns');
+
+// get return
+$returnId = 42;
+$return = $client->get("returns/$returnId");
+
+// get brands
+$brands = $client->get('brands');
+
+// get brand
+$brandId = 42;
+$brand = $client->get("brands/$brandId");
+
+// get shipping methods
+$shippingMethods = $client->get('shipping_methods');
+
+// get shipping method
+$shippingMethodId = 42;
+$shippingMethods = $client->get("shipping_methods/$shippingMethodId");
+
+// get shipping price
+$shippingMethodId = 8;
+$shippingPrices = $client->get('shipping-price', [
+    'shipping_method_id' => $shippingMethodId,
+    'from_country' => 'NL',
+    'to_country' => 'NL',
+    'weight' => 1,
+    'weight_unit' => 'kilogram'
+]);
+
+// get all shipping functionalities
+$shippingFunctionalities = $client->get('shipping-functionalities');
+
+// get shipping products
+$shippingProducts = $client->get('shipping-products', [
+    'from_country' => 'NL'
+]);
+
+
 // get label urls
 $parcelId = 42;
-$labels = $client->request(Client::METHOD_GET, "labels/$parcelId");
+$labels = $client->get("labels/$parcelId");
 
 // get label data
-$labelData = $client->request(Client::METHOD_GET, $labels['label']['normal_printer'][0]);
+$labelData = $client->get($labels['label']['normal_printer'][0]);
 
 // create label file
 $labelFile = '/path/to/filename.pdf';
 
 // write data to file
 file_put_contents($labelFile, $labelData);
+
+// bulk pdf label printing
+$labels = $client->post("labels", [
+    'label' => [
+        'parcels' => [
+            42,
+            43,
+            44
+        ]
+    ]
+]);
+
+
+// get label data
+$labelData = $client->get($labels['label']['normal_printer'][0]);
+
+// create label file
+$labelFile = '/path/to/filename.pdf';
+
+// write data to file
+file_put_contents($labelFile, $labelData);
+
+// get user
+$user = $client->get('user');
+
+// get invoices
+$invoices = $client->get('user/invoices');
+
+// get invoice
+$invoiceId = 42;
+$invoice = $client->get("user/invoices/$invoiceId");
+
+// get sender addresses
+$senderAddresses = $client->get('user/addresses/sender');
+
+// get sender address
+$senderAddressId = 42;
+$senderAddresses = $client->get("user/addresses/sender/$senderAddressId");
+
+// get integrations
+$integrations = $client->get('integrations');
+
--- src/Client.php
+++ src/Client.php
@@ -50,21 +50,74 @@ class Client
     }
     
     /**
+     * Get uri & extract query params
+     * 
      * @param string $endpoint
-     *
+     * @param array &$query = []
+     * 
      * @return string
      */
-    private function getUrl(string $endpoint): string
+    private function getUri(string $endpoint, array &$query = []): string
     {
-        $baseUri = self::BASE_HREF . '/' . $this->version;
+        // extract query params
+        $queryString = parse_url($endpoint, PHP_URL_QUERY);
+        parse_str($queryString, $queryArray);
         
+        // merge query params
+        $query = array_merge($queryArray, $query);
+        
+        // remove query string from endpoint
+        $endpoint = str_replace("?$queryString", '', $endpoint);
+        
+        // build base uri
+        $baseUri = self::BASE_HREF.'/'.$this->version;
+        
+        // add endpoint to base uri
         if(strpos($endpoint, $baseUri) === false){
-            
-            return $baseUri . '/' . $endpoint;
-            
+            $uri = "$baseUri/$endpoint";
         } else {
-            return $endpoint;
+            $uri = $endpoint;
         }
+        
+        return urldecode($uri);
+    }
+    
+    /**
+     * @param string $endpoint
+     * @param array $query = []
+     * @param array $headers = []
+     *
+     * @return mixed
+     */
+    public function get(string $endpoint, array $query = [], array $headers = [])
+    {
+        return $this->request(self::METHOD_GET, $endpoint, [], $query, $headers);
+    }
+    
+    /**
+     * @param string $endpoint
+     * @param array $data = []
+     * @param array $query = []
+     * @param array $headers = []
+     *
+     * @return mixed
+     */
+    public function post(string $endpoint, array $data = [], array $query = [], array $headers = [])
+    {
+        return $this->request(self::METHOD_POST, $endpoint, $data, $query, $headers);
+    }
+    
+    /**
+     * @param string $endpoint
+     * @param array $data = []
+     * @param array $query = []
+     * @param array $headers = []
+     *
+     * @return mixed
+     */
+    public function put(string $endpoint, array $data = [], array $query = [], array $headers = [])
+    {
+        return $this->request(self::METHOD_PUT, $endpoint, $data, $query, $headers);
     }
     
     /**
@@ -72,30 +125,43 @@ class Client
      * @param string $endpoint
      * @param array $data = []
      * @param array $query = []
+     * @param array $headers = []
      * 
-     * @return mixed array|null|string
+     * @return mixed
      */
-    public function request(string $method, string $endpoint, array $data = [], array $query = [])
+    public function request(string $method, string $endpoint, array $data = [], array $query = [], array $headers = [])
     {
+        // build headers
+        $headers = array_merge([
+            'Content-Type' => 'application/json',
+        ], $headers);
+        
+        // setup options
         $options = [
-            RequestOptions::HEADERS => [
-                'Content-Type' => 'application/json',
-            ],
-            RequestOptions::QUERY => $query,
+            RequestOptions::HEADERS => $headers,
             RequestOptions::AUTH => [
                 $this->apiKey,
                 $this->apiSecret
             ]
         ];
         
+        // add data
         if (in_array($method, [self::METHOD_POST, self::METHOD_PUT])) {
             $options[RequestOptions::JSON] = $data;
         }
         
+        // get uri & extract query from endpoint string
+        $uri = $this->getUri($endpoint, $query);
+        
+        // add query
+        $options[RequestOptions::QUERY] = $query;
+        
         try {
             
-            $response  = (new GuzzleClient())->request($method, $this->getUrl($endpoint), $options);
+            // request
+            $response = (new GuzzleClient())->request($method, $uri, $options);
             
+            // get contents
             $contents = $response->getBody()->getContents();
             
             if ($response->getHeaderLine('Content-Type') == 'application/json') {
